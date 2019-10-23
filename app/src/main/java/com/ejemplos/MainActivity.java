8c48313693b860c90f1e81ef8299c15f1cafd72f
pmdmdam2 <rmenmar264@gmail.com>
2019-10-23 08:06:41 +0200

    Initial commit

---
+++ app/src/main/java/com/ejemplos/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ejemplos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Switch swMostrar = findViewById(R.id.swMostrar);
+        final Group grMostrar = findViewById(R.id.grMostrar);
+
+        swMostrar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    grMostrar.setVisibility(View.GONE);
+                else
+                    grMostrar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
